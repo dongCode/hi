3ff9dd9c4f6a4df2a07ebdfaf80f11c6a46fb75a
Git Dong <939929159@qq.com>
2018-05-30 15:04:39 +0800

    isIdCard isPhoneNum

--- src/index.js
+++ src/index.js
@@ -134,3 +134,33 @@ function typeIs (o) {
 function classIs (o) {
   return _toString.call(o).slice(8, -1).toLowerCase()
 }
+/**
+ * @isEmail
+ * @description
+ * 判断是否为邮箱地址
+ * @param  {String}  str
+ * @return {Boolean}
+ */
+function isEmail(str) {
+  return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+}
+/**
+ * @name isIdCard
+ * @description
+ * 判断是否为身份证号
+ * @param  {String|Number} str
+ * @return {Boolean}
+ */
+function isIdCard(str) {
+  return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str)
+}
+/**
+ * @name isPhoneNum
+ * @description
+ * 判断是否为手机号
+ * @param  {String|Number} str
+ * @return {Boolean}
+ */
+function isPhoneNum(str) {
+  return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
+}
