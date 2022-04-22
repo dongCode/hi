0c8765eeb4ae9e3e6b2a31d64412a4b1b8cf7a28
new-bee <gdongcode@gmail.com>
2022-04-22 19:25:41 +0800

    refactor: redux demo

--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -1,19 +1,40 @@
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { decrement, increment } from './counterSlice';
+import { decrement, increment, incrementByAmount, incrementAsync } from './counterSlice';
+import styles from './counter.module.css';
+import { useState } from 'react';
 
 export function Counter() {
   const count = useAppSelector(state => state.counter.value);
   const dispatch = useAppDispatch();
-
+  const [incrementAmount, setIncrementAmount] = useState('2');
   return (
     <div>
-      <div>
-        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+      <div className={styles.row}>
+        <button className={styles.button} onClick={() => dispatch(increment())}>
           Increment
         </button>
-        <span>{count}</span>
-        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+        <span className={styles.value}>{count}</span>
+        <button className={styles.button} onClick={() => dispatch(decrement())}>
           Decrement
+        </button>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          value={incrementAmount}
+          onChange={e => setIncrementAmount(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}
+        >
+          Add Amount
+        </button>
+        <button
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+        >
+          Add Async
         </button>
       </div>
     </div>
