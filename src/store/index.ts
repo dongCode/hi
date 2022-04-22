e461125d27134c539a0652a288d464e018c87a42
new-bee <gdongcode@gmail.com>
2022-04-22 16:00:38 +0800

    feat: 全局状态管理

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from '../pages/counter/counterSlice';
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+});
