3f70169bfb2223494eff893527075f8f4459e943
D <gdongcode@gmail.com>
2022-04-25 21:56:54 +0800

    feat: 没用用到的变量报错

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -3,6 +3,7 @@ import { Layout, Menu } from 'bellejs';
 import { Content } from 'bellejs/lib/layout/layout';
 import Sider from 'bellejs/lib/layout/Sider';
 import { Outlet } from 'react-router-dom';
+import MenuItem from 'bellejs/lib/menu/MenuItem';
 const AppLayout = () => {
   return (
     <Layout className="g-h100 app-layout">
